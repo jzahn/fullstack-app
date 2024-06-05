c0cd0a625930fa6710d5731dbc2b78ca2051d5be
Jason Zahn <smedge@gmail.com>
2024-06-05 12:24:10 -0500

    trying a new client gen

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavigationComponent } from "./navigation/navigation.component";
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss',
-    imports: [RouterOutlet,
-      NavigationComponent
-    ]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, MatSlideToggleModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'COntact Manager';
+  title = 'client';
 }
